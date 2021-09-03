880ef4ed89bb90cfd6cb2bb1aed0675d83d538d4
iznilul <1773950094@qq.com>
2021-09-04 01:20:26 +0800

    初始化

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+import { resetRouter } from '../router'
+
+export function resetTokenAndClearUser() {
+  // 退出登陆 清除用户资料
+  localStorage.clear()
+  sessionStorage.clear()
+  // 重设路由
+  resetRouter()
+}
+
+//标签标题
+export const defaultDocumentTitle = '实验室管理系统'
+
+export function getDocumentTitle(pageTitle) {
+  if (pageTitle) return `${defaultDocumentTitle} - ${pageTitle}`
+  return `${defaultDocumentTitle}`
+}
---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import defaultSettings from '../config/defaultSettings'
+import router from '../router'
+
+export const baseURL = process.env.NODE_ENV === 'development' ? defaultSettings.baseURL.dev : defaultSettings.baseURL.prod
+
+const service = axios.create({
+  baseURL: baseURL,
+  timeout: 6000,
+})
+
+service.interceptors.request.use(
+  (config) => {
+  },
+  (error) => {
+
+  }
+)
+
+service.interceptors.response.use(
+  (response) => {
+  },
+  (error) => {
+  }
+)
+
+export default service
